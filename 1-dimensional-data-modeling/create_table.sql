1049e23580278250a12de5b2cd32423968faf711
jmcabreira <cabreirajm@gmail.com>
2025-07-09 17:11:46 -0300

    refact: Add two more columns ( scoring_class and years_since_last_season)

--- 1-dimensional-data-modeling/create_table.sql
+++ 1-dimensional-data-modeling/create_table.sql
@@ -1,4 +1,5 @@
  -- create bigquery table for cumulative dimensions 
+
 CREATE TABLE `silent-scanner-465121-j7.jmc_data.players` (
               player_name STRING, 
               player_height FLOAT64,
@@ -7,6 +8,8 @@ CREATE TABLE `silent-scanner-465121-j7.jmc_data.players` (
               draft_year STRING,
               draft_round STRING,
               draft_number STRING,
+              scoring_class STRING, 
+              years_since_last_season INT64,
               season_stats ARRAY<STRUCT<
                                     season INT64, 
                                     gp FLOAT64, 
--- 1-dimensional-data-modeling/cumulative_tb_design.sql
+++ 1-dimensional-data-modeling/cumulative_tb_design.sql
@@ -7,14 +7,17 @@ INSERT INTO `silent-scanner-465121-j7.jmc_data.players` (
   draft_round,
   draft_number,
   season_stats,
+  scoring_class, 
+  years_since_last_season,
+
   current_season
 )
 WITH yesterday AS (
     SELECT * FROM `silent-scanner-465121-j7.jmc_data.players`
-    WHERE current_season = 2001
+    WHERE current_season = 2000
 ), today AS (
       SELECT * FROM `silent-scanner-465121-j7.jmc_data.Models`
-      WHERE CAST(LEFT(season,4) AS INT64) = 2002
+      WHERE CAST(LEFT(season,4) AS INT64) = 2001
     )
 SELECT 
   COALESCE(t.player_name, y.player_name) as player_name,
@@ -36,6 +39,18 @@ SELECT
     ELSE 
       y.season_stats
   END AS season_stats,
+  CASE 
+    WHEN t.season IS NOT NULL THEN 
+       CASE WHEN t.pts > 20 then "star"
+            WHEN t.pts > 15 THEN "good"
+            WHEN t.pts > 10 THEN "average"
+      ELSE "bad"
+          END
+      ELSE y.scoring_class
+      END AS scoring_class,
+    CASE WHEN t.season IS NOT NULL THEN 0
+        ELSE y.years_since_last_season + 1
+        end as years_since_last_season,
   COALESCE(CAST(LEFT(t.season, 4) AS INT64), y.current_season + 1) AS current_season
 FROM today t FULL OUTER JOIN yesterday y 
   ON t.player_name = y.player_name
